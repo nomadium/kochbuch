f49f30d639fdcc3b364368bb82376b3eda1f00b9
Miguel Landaeta <miguel@openenglish.com>
2013-07-18 15:56:32 -0300

    Add openvpn cookbook

---
+++ site-cookbooks/openvpn/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: openvpn
+# Recipe:: default
+#
+# Copyright 2013, Miguel Landaeta
+#
+# All rights reserved - Do Not Redistribute
+#
+package "openvpn" do
+  action :install
+end
